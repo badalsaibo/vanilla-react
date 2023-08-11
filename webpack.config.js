66f557a2ead61f7dc745afd585d3d6b788f0e366
Badal Saibo <badalsaibo@gmail.com>
2023-08-11 16:53:45 +0530

    feat: html webpack implementation

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./index.js",
@@ -20,4 +21,5 @@ module.exports = {
       },
     ],
   },
+  plugins: [new HtmlWebpackPlugin({ template: "./public/index.html" })],
 };
